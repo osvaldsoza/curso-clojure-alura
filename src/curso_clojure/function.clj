770b3b8a6833d973f3ae318fa8afdad14e1e5162
osvaldsoza <osvald.soza@gmail.com>
2021-10-31 14:41:16 -0300

    map  e filter

--- src/curso_clojure/function.clj
+++ src/curso_clojure/function.clj
@@ -1,9 +1,9 @@
 (ns curso-clojure.function)
 
-;(defn aplica-desconto?
-;  [valor-bruto]
-;  (> valor-bruto 100)
-;  )
+(defn aplica-desconto?
+  [valor-bruto]
+  (> valor-bruto 100)
+  )
 
 (defn mais-caro-que-100? [valor-bruto] (> valor-bruto 100))
 
@@ -11,8 +11,8 @@
 ;(fn [valor-bruto] (> valor-bruto 100))
 
 (defn valor-descontado
-  [aplica? valor-bruto]
-  (if (aplica? valor-bruto)
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
     (let [taxa-de-desconto (/ 10 100)
           desconto (* valor-bruto taxa-de-desconto)]
       (- valor-bruto desconto)
@@ -21,14 +21,21 @@
     )
   )
 
-(println (valor-descontado mais-caro-que-100? 1000))
+(def precos [100 150 200 300 400])
 
-(println "---------------------------")
-(println "Utilizando funcoes anonimas")
-(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 1000))
-(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 100))
-;funcao anonima apenas com um parametro
-(println (valor-descontado #(> %1 100) 1000))
-(println (valor-descontado #(> % 100) 100))
+(println precos)
+
+(println (map valor-descontado precos))
+
+(println (filter aplica-desconto? precos))
+;(println (valor-descontado 1000))
+
+;(println "---------------------------")
+;(println "Utilizando funcoes anonimas")
+;(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 1000))
+;(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 100))
+;;funcao anonima apenas com um parametro
+;(println (valor-descontado #(> %1 100) 1000))
+;(println (valor-descontado #(> % 100) 100))
 
 
--- src/curso_clojure/vetores.clj
+++ src/curso_clojure/vetores.clj
@@ -3,3 +3,24 @@
 (def precos [30 70 1000])
 
 (println (precos 0))
+
+;return error indexOutOfBounds Exception
+;(println (precos 3))
+
+; se nao existe, retorna nil. o get faz um tratamento
+(println (get precos 3))
+; valor default = 0 caso nao exista
+(println (get precos 3 0))
+
+;atualizar o primeiro elemento e retorna um novo vetor
+(def precos-inc (update precos 0 inc))
+(def precos-dec (update precos 1 dec))
+(println precos-inc)
+(println precos-dec)
+(println precos)
+
+(defn soma-1
+  [valor]
+  (+ valor 1)
+  )
+(println (update precos 2 soma-1))
